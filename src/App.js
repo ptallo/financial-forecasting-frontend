af865ed270d292148cb20f0d3c8e8c9125b69e8c
Philip Tallo <tallopt@mail.uc.edu>
2020-03-22 16:04:54 -0400

    added handleAuthorization api call

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     }
 
     this.handleAuthorization = this.handleAuthorization.bind(this);
-    this.updateTicker = this.updateTicker.bind(this);
+    this.updateTicker = this.makeTickerCall.bind(this);
   }
 
   render() {
@@ -32,28 +32,40 @@ class App extends React.Component {
     }
 
     return <div className="App">
-      <Navbar loggedIn={this.state.loggedIn} searchHandler={this.updateTicker} loginHandler={this.handleAuthorization}></Navbar>
+      <Navbar loggedIn={this.state.loggedIn} searchHandler={this.makeTickerCall} loginHandler={this.handleAuthorization}></Navbar>
       {graph}
     </div>
   }
 
   handleAuthorization(username, password) {
-    alert(`${username} ${password}`);
+    let data = {
+      username: username,
+      password: password
+    }
+
+    fetch(`${this.baseHTML}/login/`, {
+      method: 'GET',
+      mode: 'cors',
+      headers : {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      }, (error) => {
+        console.log(error);
+      });
   }
 
-  updateTicker(ticker) {
+  makeTickerCall(ticker) {
     const startDate = '2019-10-08';
     const endDate = '2019-10-22';
 
-    this.setState({
-      stockInfo: {
-        companyName: '',
-        ticker: '',
-        x: [],
-        y: [[]],
-        names: []
-      }
-    })
+    this.updateTicker('', 'AAPL', [], [[]], [])
 
     fetch(`${this.baseHTML}/?stock=${ticker}&start=${startDate}&end=${endDate}`, {
       method: 'GET',
@@ -63,18 +75,22 @@ class App extends React.Component {
         return response.json();
       })
       .then((json) => {
-        this.setState({
-          stockInfo: {
-            companyName: 'filler',
-            ticker: ticker,
-            x: json.x,
-            y: json.y,
-            names: json.names
-          }
-        });
+        this.updateTicker('filler', ticker, json.x, json.y, json.names)
       }, (error) => {
         alert(error.message);
       });
+  }
+
+  updateTicker(companyName, ticker, x, y, names) {
+    this.setState({
+      stockInfo: {
+        companyName: companyName,
+        ticker: ticker,
+        x: x,
+        y: y,
+        names: names
+      }
+    })
   }
 }
 
